3b4876cb7aea6cc59177f1ecb96028991a97ace7
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-01-12 19:33:09 +0000

    more styling + js

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,20 @@
-$(".search-button").on("click", function () {
-
+//establish global variables
+const searchBtnEl = $("#search-button");
+const inputEl = document.querySelector('#search-input');
+const citiesEl = document.querySelector('list-group');
+const mainCard = document.querySelector('.cityDisplay')
 const apiKey = "3b7dc2a8e8d5e5a94b1ce0e837157663"
 const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
+searchBtnEl.addEventListener("submit", event => {
+
 
 fetch(queryURL).then(function(response){
   return response.json();
 })
 
-const inputEl = document.querySelector('.search-input');
-const searchBtnEl = document.querySelector('.search-button');
-const citiesEl = document.querySelector('list-group');
+
+
 
 // Hint: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
 
